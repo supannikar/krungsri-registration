76f7312fc01d30cfc388cd1efc0ac554f053ca49
ni <ni@bluestonepim.com>
2022-10-17 22:46:10 +0700

    Remove unused import

--- src/main/java/com/krungsri/controller/AuthController.java
+++ src/main/java/com/krungsri/controller/AuthController.java
@@ -1,7 +1,5 @@
 package com.krungsri.controller;
 
-import com.krungsri.exception.InvalidCredentialException;
-import com.krungsri.exception.UsernameNotFoundException;
 import com.krungsri.model.JwtTokenResponse;
 import com.krungsri.model.RegistrationRequest;
 import com.krungsri.model.RegistrationResponse;
@@ -16,15 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @Api(tags = "Authentication")
 @RestController
